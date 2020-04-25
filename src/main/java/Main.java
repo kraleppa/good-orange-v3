6a2b3804b1e8e42f4c425e124ea2ea63cd601f24
kraleppa <misiek.nalepa@wp.pl>
2020-04-25 15:08:00 +0200

    Main del

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import service.CreateQuizService;
 import visualization.GUI;
 
-// https://pastebin.pl/view/20ffd07c
 public class Main {
     public static void main(String[] args){
         GUI gui = new GUI();
